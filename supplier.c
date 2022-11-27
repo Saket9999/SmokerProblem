f2af9c1174fe8d6e1d20ae97514cb4eb4ec7e8d0
SaketV <115332760+Saket9999@users.noreply.github.com>
2022-11-28 00:04:45 +0530

    Update supplier.c

--- supplier.c
+++ supplier.c
@@ -6,11 +6,10 @@
 #include<stdio.h>
 
 union semun {
-               int              val;    /* Value for SETVAL */
-               struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-               unsigned short  *array;  /* Array for GETALL, SETALL */
-               struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                                           (Linux-specific) */
+               int              val;   
+               struct semid_ds *buf;    
+               unsigned short  *array;  
+               struct seminfo  *__buf;  
            }a,b;
 
 int main(int argc, char *argv[])
@@ -21,24 +20,17 @@ int main(int argc, char *argv[])
 	key_t k = 1016;	
 	
 	id= semget(k,  6, IPC_CREAT|0777);
-/*	arr[atoi(argv[1])]=1;
-	arr[atoi(argv[2])]=1;
-	*/
+
 		a.array=arr;
 	
 			
-	p2.sem_num=atoi(argv[1]);  /* semaphore number */
-	p2.sem_op=+1;   /* semaphore operation */
+	p2.sem_num=atoi(argv[1]);  
+	p2.sem_op=+1;  
 	p2.sem_flg=0;
 	
-	p3.sem_num=atoi(argv[2]);  /* semaphore number */
-	p3.sem_op=+1;   /* semaphore operation */
+	p3.sem_num=atoi(argv[2]);  
+	p3.sem_op=+1;   
 	p3.sem_flg=0;
-	
-/*	p4.sem_num=2;  
-	p4.sem_op=+1;  
-	p4.sem_flg=0;
-	*/
 	 
 	if (id == -1)
 	{
@@ -75,4 +67,3 @@ int main(int argc, char *argv[])
 	}
 }
         
-// (0,4) (4,3) (3,2) (2, 1) (1,0) 
