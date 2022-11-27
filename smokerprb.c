ce83963a0ea8ef20b94df78b00f4ba53880f15d6
SaketV <115332760+Saket9999@users.noreply.github.com>
2022-11-28 00:02:35 +0530

    Update smokerprb.c

--- smokerprb.c
+++ smokerprb.c
@@ -12,36 +12,28 @@ int main(int argc, char *argv[])
 	int len,val,i=0;
 	int left,right,id;
 	char buf[30];
-	//a=0;
-	//b=4;
+	
 	key_t k = 1016;	
  	struct  sembuf p1,p5,p2,p4;
 	time_t t;
 	
-/*	if (argc !=4)
-	{
-//		write(1,"Need Argument 1 : Name",35); write(1,"\n",1);
-//		write(1,"Need Argument 2 : ",32); write(1,"\n",1);
-//		write(1,"Need Argument 3 : Right Fork no.",33);	write(1,"\n",1);	
-		exit(0);
-	}	*/
 	left=atoi(argv[2]);	
 	right=atoi(argv[3]);
-	p1.sem_num=left;  /* semaphore number */
-	p1.sem_op=-1;   /* semaphore operation */
-	p1.sem_flg=0;  /* operation flags */
+	p1.sem_num=left;  
+	p1.sem_op=-1;  
+	p1.sem_flg=0; 
 
-	p5.sem_num=right;  /* semaphore number */
-	p5.sem_op=-1;   /* semaphore operation */
- 	p5.sem_flg=0;  /* operation flags */
+	p5.sem_num=right; 
+	p5.sem_op=-1;  
+ 	p5.sem_flg=0; 
 
-	p2.sem_num=left;  /* semaphore number */
-	p2.sem_op=+1;   /* semaphore operation */
-	p2.sem_flg=0;  /* operation flags */
+	p2.sem_num=left; 
+	p2.sem_op=+1;  
+	p2.sem_flg=0;  
 
-	p4.sem_num=right;  /* semaphore number */
-	p4.sem_op=+1;   /* semaphore operation */
- 	p4.sem_flg=0;  /* operation flags */
+	p4.sem_num=right;  
+	p4.sem_op=+1;  
+ 	p4.sem_flg=0;  
 
 
 
@@ -52,8 +44,6 @@ int main(int argc, char *argv[])
 		write(1, "Create Error", 12);
 		exit(0);
 	}
-//	len=sprintf(buf,"Descriptor: %d",id);
-//	write(1,buf,len);
 
 	while (1)
 	{
@@ -61,12 +51,8 @@ int main(int argc, char *argv[])
 		write(1, argv[1], sizeof(argv[1])+2 );
 		write(1,"\n",1);
 		write(1,"^^^^^^^^^^^^^^^",15);
-//		write(1,"=====WAITING====", 16);
 		write(1,"\n",1);
 			
-//		sleep(rand()%3);
-
-//		write(1,"=====SMOKING=====", 16);
 		if(left==0 && right==1)
 		{
 			write(1,"Cigarette is available\n",23);
@@ -80,11 +66,8 @@ int main(int argc, char *argv[])
 			write(1,"Match is available\n",19);
 		}
 		
-//		write(1,"\n",1);
-		
 		sleep(rand()%3);
-		
-		
+	
 		val=semop(id,&p1,1);
 		if (val == -1 )
 		{
@@ -100,20 +83,7 @@ int main(int argc, char *argv[])
 			write(1,"Cigarette is available",23);
 		}
 		
-//		write(1,"Taken the Left Fork ", 20);
-//		write(1,"\n",1);
-		//val= semctl(id,0, GETVAL, NULL);
-		//len=sprintf(buf,"Sem 0 Before : %d",val);
-		//write(1,buf,len);		
-		//write(1,"\n", 1);
-
-		
 		sleep(rand()%3);
-/*		if(argv[1]==0 && argv[2]==1)
-		{
-			write(1,"Match is available\n",17);
-		}	*/
-		
 		
 		val=semop(id,&p5,1);
 		write(1,"\n",1);
@@ -130,50 +100,15 @@ int main(int argc, char *argv[])
 		{
 			write(1,"Cigarette is available\n",23);
 		}
-//		write(1,"Taken the Right Fork ", 20);
-//		write(1,"\n",1);
-		//val= semctl(id,4, GETVAL,NULL);
-		//len=sprintf(buf,"Sem 4 Before : %d",val);
-		//write(1,buf,len);		
-		//write(1,"\n", 1);		
+	
 		write(1,"\n",1);
 		write(1,"====SMOKING=====", 16);
 		write(1,"\n",1);
-//		time(&t);
-///		printf("%d Time at %s", i+1, ctime(&t));
-//		write(1,"\n",1);
-		//len=sprintf(buf,"Descri:  %d",id);
-		//write(1,"\n",1);
-		//write(1,buf,len);	
 		
 		sleep(7);
-		
-/*		val=semop(id,&p2,1);
-	 	if (val ==-1)
-		{
-			write(1, "Error", 16);
-			exit(0);
-		}
-		write(1,"Put down the Left Fork", 23);
-		write(1,"\n",1);
-		
-		//val= semctl(id,0, GETVAL, NULL);
-		//len=sprintf(buf,"Sem 0 After : %d",val);
-		//write(1,buf,len);		
-	        //write(1,"\n", 1);*/
+	
 
 		sleep(rand()%3);				
-//		val=semop(id,&p4,1);
-/*	 	if (val == -1 )
-		{
-			write(1, "Right fork Error", 16);
-			exit(0);
-		}
-		write(1,"Put down the Right Fork", 24);
-		write(1,"\n",1);*/
-		//val= semctl(id,4, GETVAL, NULL);
-		//len=sprintf(buf,"Sem 4 After : %d",val);
-		//write(1,buf,len);		
 		write(1,"\n", 1);
 		i++;
 		
